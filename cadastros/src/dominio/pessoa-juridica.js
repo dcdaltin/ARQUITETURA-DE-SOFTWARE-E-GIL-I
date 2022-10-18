3236771c096de8d3924f10de906e83607b4ca75e
carolinanog <carolina.tinen@alumni.usp.br>
2022-10-17 21:19:32 -0300

    Gi e Carol

--- cadastros/src/dominio/pessoa-juridica.js
+++ cadastros/src/dominio/pessoa-juridica.js
@@ -1,4 +1,5 @@
-const Pessoa = require("./pessoa.js")
+const Pessoa = require("./pessoa.js");
+const pessoaEvento = require('../eventos/pessoa.evento.js');
 
 class PessoaJuridica extends Pessoa {
     cnpj
@@ -6,6 +7,31 @@ class PessoaJuridica extends Pessoa {
         super(nome, idade, dataNascimento)
         this.cnpj = cnpj
     }
-}
+    static criar(nome, contato, dataNascimento, cnpj) {
+        const resultadoValidacaoCnpj = PessoaJuridica.validarCnpj(cnpj)
+        const PessoaJuridica = new PessoaJuridica(nome, contato, dataNascimento, resultadoValidacaoCnpj)
+        pessoaEvento.emit("pessoa-juridica.criada", pessoaJuridica)
+        return pessoaJuridica
+    }
 
+    static atualizar(pessoaJuridica) {
+        const resultadoValidacaoCnpj = PessoaJuridica.validarCnpj(pessoaJuridica.cnpj)
+        const novaPessoaJuridica = new PessoaJuridica(pessoaJuridica.nome, pessoaJuridica.contato, pessoaJuridica.dataNascimento, resultadoValidacaoCnpj)
+        pessoaEvento.emit("pessoa-juridica.atualizada", novaPessoaJuridica)
+        return novaPessoaJuridica
+    }
+
+    static validarCnpj(cnpj) {
+        const formataCnpjString = String(cnpj)
+        const cnpjFormatado = `${formataCnpjString.substring(0, 2)}.${formataCnpjString.substring(2, 5)}.${formataCnpjString.substring(5, 8)}/${formataCnpjString.substring(8, 12)}-${formataCnpjString.substring(12, 14)}`
+        const validar = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/;
+        const resultado = cnpjFormatado.match(validar)
+        if (!resultado) {
+            throw new Error('CNPJ inválido')
+        }
+        return formataCnpjString
+    }
+
+}
 module.exports = PessoaJuridica
+
