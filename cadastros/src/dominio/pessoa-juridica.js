2de9486d4e803ca1ab38680199f4477986c07fa8
Alize <alize.de.fatima@gmail.com>
2022-10-17 21:33:53 -0300

    Alteração do dominio/pessoa-juridica.js

--- cadastros/src/dominio/pessoa-juridica.js
+++ cadastros/src/dominio/pessoa-juridica.js
@@ -8,4 +8,29 @@ class PessoaJuridica extends Pessoa {
     }
 }
 
+static criar(nome, contato, dataNascimento, cnpj) {
+    const resultadoValidacaoCnpj = PessoaJuridica.validarCnpj(cnpj)
+    const pessoaJuridica = new PessoaJuridica(nome, contato, dataNascimento, resultadoValidacaoCnpj)
+    pessoaEvento.emit("pessoa-juridica.criada", pessoaJuridica)
+    return pessoaJuridica
+}
+
+// static atualizar(pessoaJuridica) {
+//     const resultadoValidacaoCnpj = PessoaJuridica.validarCnpj(pessoaFisica.cpf)
+//     const novaPessoaJuridica = new PessoaJuridica(pessoaJuridica.nome, pessoaJuridica.contato, pessoaJuridica.dataNascimento, resultadoValidacaoCnpj)
+//     pessoaEvento.emit("pessoa-juridica.atualizada", novaPessoaJuridica)
+//     return novaPessoaJuridica
+// }
+
+static validarCnpj(cnpj) {
+    const formataCnpjString = String(cnpj)
+    const cnpjFormatado = `${formataCnpjString.substring(0, 2)}.${formataCnpjString.substring(2, 5)}.${formataCnpjString.substring(5, 8)}/${formataCnpjString.substring(8, 12)}-${formataCnpjString.substring(12, 14)}`
+    const validar = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}\$/;
+    const resultado = cnpjFormatado.match(validar)
+    if (!resultado) {
+        throw new Error("Cnpj invalido")
+    }
+    return formataCnpjString
+}
+
 module.exports = PessoaJuridica
