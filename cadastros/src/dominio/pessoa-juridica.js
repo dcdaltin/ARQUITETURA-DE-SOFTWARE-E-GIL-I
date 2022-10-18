80f5a675011323fc25a9e093c307d54bdde46f1e
Kaf278 <karenf.hxcx@gmail.com>
2022-10-17 21:09:48 -0300

    validaçao pessoa juridica

--- cadastros/src/dominio/pessoa-juridica.js
+++ cadastros/src/dominio/pessoa-juridica.js
@@ -1,11 +1,42 @@
-const Pessoa = require("./pessoa.js")
+const Pessoa = require("./pessoa.js");
 
 class PessoaJuridica extends Pessoa {
-    cnpj
-    constructor(nome, idade, dataNascimento, cnpj) {
-        super(nome, idade, dataNascimento)
-        this.cnpj = cnpj
+  cnpj;
+  constructor(nome, idade, dataNascimento, cnpj) {
+    super(nome, idade, dataNascimento);
+    this.cnpj = cnpj;
+  }
+
+  static criar(nome, idade, dataNascimento, cnpj) {
+    const resultadoValidacaoCnpj = PessoaJuridica.validarCnpj(cnpj);
+    const pessoaJuridica = new PessoaJuridica(
+      nome,
+      idade,
+      dataNascimento,
+      resultadoValidacaoCnpj
+    );
+    pessoaEvento.emit("pessoa-juridica.criada", pessoaJuridica);
+    return pessoaJuridica;
+  }
+
+  static validarCnpj(cnpj) {
+    const formataCnpjString = String(cnpj);
+    const cnpjFormatado = `${formataCnpjString.substring(
+      0,
+      2
+    )}.${formataCnpjString.substring(2, 5)}.${formataCnpjString.substring(
+      5,
+      8
+    )}/${formataCnpjString.substring(8, 12)}-${formataCnpjString.substring(
+      12,
+      14
+    )}`;
+    const validar = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/;
+    const resultado = cnpjFormatado.match(validar);
+    if (!resultado) {
+      throw new Error("Cnpj invalido");
     }
+  }
 }
 
-module.exports = PessoaJuridica
+module.exports = PessoaJuridica;
